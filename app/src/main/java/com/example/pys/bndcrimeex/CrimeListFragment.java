cf4cd7ae54c08516df484644d4a3c5698a779546
pys <ppro311@naver.com>
2017-09-01 18:54:57 +0900

    0901

--- app/src/main/java/com/example/pys/bndcrimeex/CrimeListFragment.java
+++ app/src/main/java/com/example/pys/bndcrimeex/CrimeListFragment.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by pys on 2017. 8. 31..
@@ -16,7 +20,7 @@ import android.view.ViewGroup;
 public class CrimeListFragment extends Fragment{
 
     private RecyclerView crimeRecyclerView;
-
+    private CrimeAdapter adapter;
 
 
     @Nullable
@@ -25,17 +29,72 @@ public class CrimeListFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_crime_list,container,false);
         crimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
-        crimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        crimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));   // getActivty() 프래그먼트 호스팅하고 있는 액티비티 가져옴
 
-
+        updateUI();
 
 
         return view;
     }
 
+    private void updateUI(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        adapter = new CrimeAdapter(crimes);
+        crimeRecyclerView.setAdapter(adapter);
+
+    }
+
     private class CrimeHolder extends RecyclerView.ViewHolder{
+
+        private TextView titleTextView;
+        private TextView dateTextView;
+        private CheckBox solveCheckbox;
+        private Crime crime;
         public CrimeHolder(View itemView) {
             super(itemView);
+            titleTextView = (TextView) itemView.findViewById(R.id.titleText);
+            dateTextView = (TextView) itemView.findViewById(R.id.dataText);
+            solveCheckbox = (CheckBox) itemView.findViewById(R.id.checkSolve);
+
+        }
+
+        public void bindCrime(Crime crime){
+            this.crime = crime;
+            titleTextView.setText(crime.getTitle());
+            dateTextView.setText(crime.getDate().toString());
+            solveCheckbox.setChecked(crime.isSolved());
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+
+        private List<Crime> crimes;
+
+        public CrimeAdapter(List<Crime> crimes){
+            this.crimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_crime,parent,false);   // 인자1.프래그먼트뷰 2.뷰의 부모 3. false는 호스팅 액티비티에서 뷰를 추가한다는뜻
+
+            return new CrimeHolder(view);
+        }
+
+
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = crimes.get(position);
+            holder.bindCrime(crime);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return crimes.size();
         }
     }
 
